ce53a5e476a1f21893fc2d7c8528fd71acfac487
Codey Winslow <codeywinslow@gmail.com>
2025-02-17 15:07:18 -0800

    register game director to game manager, and add chaos system
    also add to gitignore the personal folder

---
+++ godot/brackeyssnipergame/scripts/resources/res_chaos_config.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name ChaosConfig
+
+@export var initial_chaos : int = 0
+@export var chaos_threshold : int = 100
+@export var decay_amount : int = 1
+## amount of time between decay events
+@export var decay_time : float = 1.0
