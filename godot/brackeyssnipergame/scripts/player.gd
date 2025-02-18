6268f11a4295ebfd24d5b1076372a11501b79a51
LeviathanLevi <leutholdlevi@gmail.com>
2025-02-17 22:50:55 -0800

    Added zooming to the crossbow

--- godot/brackeyssnipergame/scripts/player.gd
+++ godot/brackeyssnipergame/scripts/player.gd
@@ -7,11 +7,19 @@ var mouse_sensitivity = 0.15
 
 signal zoom_state_changed(is_zoomed: bool)
 
+var zoom_enabled = false
+var target_fov : float = 60.0  # Default zoom level
+
 # rotation limits (in degrees)
 var min_pitch = -80
 var max_pitch = 80
 var min_yaw = -90
 var max_yaw = 90
+
+#zoom:
+var zoom_speed : float = 5.0  # Speed of zoom
+var min_fov : float = 30.0    # Minimum zoom (field of view)
+var max_fov : float = target_fov    # Maximum zoom (field of view)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -33,7 +41,27 @@ func _unhandled_input(event) -> void:
 		new_pitch_rotation = clamp(new_pitch_rotation, min_pitch, max_pitch)
 		camera.rotation_degrees.x = new_pitch_rotation
 		
+	if event.is_action_pressed("zoom_in"):
+		if zoom_enabled:
+			# Zoom in (decrease FOV)
+			target_fov = max(target_fov - zoom_speed, min_fov)	
+		
+	if event.is_action_pressed("zoom_out"):
+		if zoom_enabled:
+			# Zoom out (increase FOV)
+			target_fov = min(target_fov + zoom_speed, max_fov)
+		
+	# Smoothly transition FOV
+	camera.fov = lerp(camera.fov, target_fov, 0.1)  # Smooth transition (adjust the lerp speed)
+		
 	if event.is_action_pressed("zoom"):
+		print("Zoom pressed")
+		zoom_enabled = true
 		zoom_state_changed.emit(true)
+		target_fov = max_fov - 10
+		camera.fov = target_fov
 	elif event.is_action_released("zoom"):
+		zoom_enabled = false
 		zoom_state_changed.emit(false)
+		target_fov = max_fov
+		camera.fov = target_fov
